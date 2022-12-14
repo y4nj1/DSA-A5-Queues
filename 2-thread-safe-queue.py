593645a10c4783bdb411ae7d1d2a15521da66272
Gian Carlo Estrella <giancarlobestrella@iskolarngbayan.pup.edu.ph>
2022-12-14 16:51:58 +0800

    Added Consumer and Producer threads to run the code

--- 2-thread-safe-queue.py
+++ 2-thread-safe-queue.py
@@ -37,6 +37,22 @@ PRODUCTS = (
 
 def main(args):
     buffer = QUEUE_TYPES[args.queue]()
+    producers = [
+        Producer(args.producer_speed, buffer, PRODUCTS)
+        for _ in range(args.producers)
+    ]
+    consumers = [
+        Consumer(args.consumer_speed, buffer) for _ in range(args.consumers)
+    ]
+
+    for producer in producers:
+        producer.start()
+
+    for consumer in consumers:
+        consumer.start()
+
+    view = View(buffer, producers, consumers)
+    view.animate()
 
 def parse_args():
     parser = argparse.ArgumentParser()
