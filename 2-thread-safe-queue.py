97351c44d4ca0e94551571e4f738976c84a1ec36
Gian Carlo Estrella <giancarlobestrella@iskolarngbayan.pup.edu.ph>
2022-12-14 16:44:28 +0800

    Added classes Producer and Consumer

--- 2-thread-safe-queue.py
+++ 2-thread-safe-queue.py
@@ -1,7 +1,7 @@
 import argparse
 import threading
 from queue import LifoQueue, PriorityQueue, Queue
-from random import randint
+from random import randint, choice
 from time import sleep
 from itertools import zip_longest
 
@@ -75,6 +75,26 @@ class Worker(threading.Thread):
         for _ in range(100):
             sleep(delay / 100)
             self.progress += 1
+
+class Producer(Worker):
+    def __init__(self, speed, buffer, products):
+        super().__init__(speed, buffer)
+        self.products = products
+
+    def run(self):
+        while True:
+            self.product = choice(self.products)
+            self.simulate_work()
+            self.buffer.put(self.product)
+            self.simulate_idle()
+
+class Consumer(Worker):
+    def run(self):
+        while True:
+            self.product = self.buffer.get()
+            self.simulate_work()
+            self.buffer.task_done()
+            self.simulate_idle()
 
 class View:
     def __init__(self, buffer, producers, consumers):
