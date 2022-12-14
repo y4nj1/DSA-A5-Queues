7d6a6ef86878878d6094a2e020005fc3d3af7b23
Gian Carlo Estrella <giancarlobestrella@iskolarngbayan.pup.edu.ph>
2022-12-14 14:36:33 +0800

    Refactored the program code using a Mixin class

--- 1_4-priority-queue.py
+++ 1_4-priority-queue.py
@@ -3,7 +3,31 @@ from heapq import heappop, heappush
 from dataclasses import dataclass
 from itertools import count
 
-class PriorityQueue:
+class IterableMixin:
+    def __len__(self):
+        return len(self._elements)
+
+    def __iter__(self):
+        while len(self) > 0:
+            yield self.dequeue()
+
+class Queue(IterableMixin):
+    def __init__(self, *elements):
+        self._elements = deque(elements)
+
+    def enqueue(self, element):
+        self._elements.append(element)
+
+    def dequeue(self):
+        return self._elements.popleft()
+
+
+class Stack(Queue):
+    def dequeue(self):
+        return self._elements.pop()
+
+
+class PriorityQueue(IterableMixin):
     def __init__(self):
         self._elements = []
         self._counter = count()
@@ -14,7 +38,7 @@ class PriorityQueue:
 
     def dequeue(self):
         return heappop(self._elements)[-1]
-
+        
 @dataclass
 class Message:
     event: str
