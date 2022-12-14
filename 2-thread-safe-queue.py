d6dc14d26b364916a0a4a5989ed566e7a02ef58c
Gian Carlo Estrella <giancarlobestrella@iskolarngbayan.pup.edu.ph>
2022-12-14 16:04:30 +0800

    Added Worker class with the help of threading module

--- 2-thread-safe-queue.py
+++ 2-thread-safe-queue.py
@@ -1,4 +1,5 @@
 import argparse
+import threading
 from queue import LifoQueue, PriorityQueue, Queue
 
 QUEUE_TYPES = {
@@ -6,24 +7,6 @@ QUEUE_TYPES = {
     "lifo": LifoQueue,
     "heap": PriorityQueue
 }
-
-def main(args):
-    buffer = QUEUE_TYPES[args.queue]()
-
-def parse_args():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-q", "--queue", choices=QUEUE_TYPES, default="fifo")
-    parser.add_argument("-p", "--producers", type=int, default=3)
-    parser.add_argument("-c", "--consumers", type=int, default=2)
-    parser.add_argument("-ps", "--producer-speed", type=int, default=1)
-    parser.add_argument("-cs", "--consumer-speed", type=int, default=1)
-    return parser.parse_args()
-
-if __name__ == "__main__":
-    try:
-        main(parse_args())
-    except KeyboardInterrupt:
-        pass
 
 PRODUCTS = (
     ":balloon:",
@@ -42,3 +25,31 @@ PRODUCTS = (
     ":thread:",
     ":yo-yo:",
 )
+
+def main(args):
+    buffer = QUEUE_TYPES[args.queue]()
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-q", "--queue", choices=QUEUE_TYPES, default="fifo")
+    parser.add_argument("-p", "--producers", type=int, default=3)
+    parser.add_argument("-c", "--consumers", type=int, default=2)
+    parser.add_argument("-ps", "--producer-speed", type=int, default=1)
+    parser.add_argument("-cs", "--consumer-speed", type=int, default=1)
+    return parser.parse_args()
+
+class Worker(threading.Thread):
+    def __init__(self, speed, buffer):
+        super().__init__(daemon=True)
+        self.speed = speed
+        self.buffer = buffer
+        self.product = None
+        self.working = False
+        self.progress = 0
+
+if __name__ == "__main__":
+    try:
+        main(parse_args())
+    except KeyboardInterrupt:
+        pass
+
